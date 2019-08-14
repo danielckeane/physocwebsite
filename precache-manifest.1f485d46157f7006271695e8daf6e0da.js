ae8a60eb0f1fe9bf5b254f9d381be9ba82f05df9
WizardOhio24 <rdmenzies2000@gmail.com>
2019-08-14 13:47:40 +0100

    Updates

---
+++ precache-manifest.1f485d46157f7006271695e8daf6e0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PhysocWebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e237f4658f3b63b0ac31",
+    "url": "/PhysocWebsite/static/js/runtime~main.8a51f80b.js"
+  },
+  {
+    "revision": "b39ae8bd4932bfa43fe0",
+    "url": "/PhysocWebsite/static/js/main.269eee43.chunk.js"
+  },
+  {
+    "revision": "1ca90a9a1e7474367604",
+    "url": "/PhysocWebsite/static/js/2.cfbe434d.chunk.js"
+  },
+  {
+    "revision": "b39ae8bd4932bfa43fe0",
+    "url": "/PhysocWebsite/static/css/main.2c1d1411.chunk.css"
+  },
+  {
+    "revision": "4af02156fa82cba974ef7e8fec03a82f",
+    "url": "/PhysocWebsite/index.html"
+  }
+];
