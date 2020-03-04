41a16571f03803c0517bde139b699488e1312a38
WizardOhio24 <rdmenzies2000@gmail.com>
2020-03-04 16:24:13 +0000

    Updates

---
+++ precache-manifest.bcc8442fbdf3965a449a831f4e7b6787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4d3cf2e82a24dee54a2",
+    "url": "/static/js/main.22793ce2.chunk.js"
+  },
+  {
+    "revision": "2fb18ed6a2793c16129e",
+    "url": "/static/js/2.66887877.chunk.js"
+  },
+  {
+    "revision": "e4d3cf2e82a24dee54a2",
+    "url": "/static/css/main.cb5c138b.chunk.css"
+  },
+  {
+    "revision": "4fd0871d6e2aad5e66041d309f085514",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PhysocWebsite/precache-manifest.3f5075dd2c1e2812d0c01a512b76817c.js"
+  "/precache-manifest.bcc8442fbdf3965a449a831f4e7b6787.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/PhysocWebsite/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
