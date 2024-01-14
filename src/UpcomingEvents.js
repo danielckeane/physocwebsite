bc106fb552faa45096158da2c606fff8097ef9c6
tia <christianemayer5@gmail.com>
2024-01-14 14:15:30 +0000

    changed upcoming events

--- src/UpcomingEvents.js
+++ src/UpcomingEvents.js
@@ -17,39 +17,23 @@ class UpcomingEvents extends Component {
           {" Upcoming Events "}
         </Typography>
 
-        <EventItem
-                    img="images/RedQuark3.svg"
-                    date="Tuesday 14/11 - 6pm"
-                    title="LaTeX Tutorial"
-                    location="Kelvin Building Computer Cluster 333"
-                    body="A relaxed tutorial for complete beginners! No equipment needed!"
-                />
 
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdVlgRM6s7NJb3eiT5IUzoV2Jd-kakyxQIUWHDxbO00oWoXHQ/viewform" style={{textDecoration: "none", color: "#ecefe4"}}>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeW4-LAVmxwW0CnKsPs-uHzVy5JyHfSOFgx_EVzexVfgLCQtw/viewform" style={{textDecoration: "none", color: "#ecefe4"}}>
         <EventItem
                     img="images/RedQuark3.svg"
-                    date="Tuesday 21/11 - 8pm"
-                    title="WiSTEM x PhySoc Christmas Ceilidh"
-                    location="QMU hall"
-                    body="PhySoc and WiSTEM are joining forces to bring you a fun end of semester ceilidh! Tickets are £8.50 and can be bought by clicking on this event! WiSTEM will be donating any profits to Glasgow Women's Aid!"
+                    date="Thursday 18/01 - 8pm"
+                    title="Burns Night Ceilidh"
+                    location="GUU Dining Hall"
+                    body="Start this semester with a fun dance!"
                 /></a>
 
         <EventItem
                     img="images/RedQuark3.svg"
-                    date="Wednesday 22/11 - 6pm"
-                    title="'The Wibbly-Wobbly Timey-Wimey Physics of Doctor Who' - by Peter H. Sneddon"
-                    location="Adam Smith Building 915"
-                    body="Time travel, space travel, mad computers intent on taking over the world... for very nearly 60 years Dr Who has embraced the “science” in “science-fiction”. In this presentation, we’ll look at a selection of times that physics has played a part in the Doctors adventures. We’ll look at how time travel could work, just how much thrust do you need to elevate a Dalek, could a Sonic Screwdriver work and more..."
+                    date="TBC"
+                    title="7 Minutes of Science"
+                    location="TBC"
+                    body="Join us for a conference-style event to hear about the exciting research being done in the various departments of our university!"
                 />
-        <a href="https://www.glasgowsciencecentre.org/whats-on/science-lates-winter-wonderland" style={{textDecoration: "none", color: "#ecefe4"}}>
-        <EventItem
-                    img="images/RedQuark3.svg"
-                    date="Saturday 09/12 - 6pm"
-                    title="Science Lates - Winter Wonderland"
-                    location="Glasgow Science Center"
-                    body="Join us for some sciency fun after-hours! We'll be selling some subsidised tickets for members only - if you are a member, you'll be receiving a ticket link via email! Of course you are also welcome to get your own ticket and still join us! Click on this event for more info!"
-                /></a>
-
         </div>
     );
   }
